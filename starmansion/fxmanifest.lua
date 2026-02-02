1c879c3d029768adb198a06d4cf1a6ebd6b6a8b0
anonymous <salt774salt@gmail.com>
2026-02-02 18:27:53 +1030

    starmansion

---
+++ starmansion/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'adamant'
+games {'gta5'}
+this_is_a_map 'yes'
+
+
+
+data_file 'AUDIO_GAMEDATA' 'audio/starkmansion_interior1_game.dat'
+data_file 'AUDIO_GAMEDATA' 'audio/starkmansion_interior1_mix.dat'
+data_file 'GTXD_PARENTING_DATA' 'gtxd.meta'
+
+files {
+	'audio/starkmansion_interior1_game.dat151.rel',
+	'audio/starkmansion_interior1_mix.dat15.rel',
+	'gtxd.meta',
+}
+
+
+
+
