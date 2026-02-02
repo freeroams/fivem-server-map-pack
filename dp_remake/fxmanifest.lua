83e9003e8d8706c6b7e2ef0c7180f2bf74d3bb3e
anonymous <salt774salt@gmail.com>
2026-02-02 20:32:37 +1030

    djdp

---
+++ dp_remake/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/designer_penthouse.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/designer_penthouse_garage.ytyp'
