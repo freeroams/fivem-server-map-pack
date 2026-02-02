30da31652e502d1792eafc7523351b8e3ce0d172
anonymous <salt774salt@gmail.com>
2026-02-02 20:31:22 +1030

    dnx

---
+++ dnxbigbuilding/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxbigbuilding_doors.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxbigbuilding_ext.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxbigbuilding_int.ytyp'
---
+++ dnxgenericaddonprops/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_buildings.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_effects.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_electronics.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_entertainment.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_firefighting.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_furniture.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_home.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_lighting.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_music.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxprops_tech.ytyp'
