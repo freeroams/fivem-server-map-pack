0a2d58e666a6fce71c2db5c2b4ac44c6d7620823
anonymous <salt774salt@gmail.com>
2026-02-02 20:35:15 +1030

    more

---
+++ nature_weekend_house_1a/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/nwh_ext_1a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/nwh_ext_1a_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/nwh_int_1a.ytyp'
---
+++ ocean-blue-villa/fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+games { 'gta5' }
+files {
+	'common/*.meta'
+}
