3efe9613448e6fdf7ed9fce6ef0240c6e2b3ead8
anonymous <salt774salt@gmail.com>
2026-02-02 20:34:30 +1030

    mmmm

---
+++ malibu_mansion/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/malibu_mansion.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/malibu_mansion_garage.ytyp'
---
+++ modern_wood_house_1a/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/mwh_ext_1a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mwh_ext_1a_lod.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mwh_garage_int_1a.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/mwh_int_1a.ytyp'
