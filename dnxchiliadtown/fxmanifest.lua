9d4eebff0a6641882577236c1f79a7598f7395c7
anonymous <salt774salt@gmail.com>
2026-02-02 19:35:59 +1030

    dnx

---
+++ dnxchiliadtown/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+files {
+	'common/dlctext.meta',
+	'common/*.meta'
+}
+data_file 'DLCTEXT_FILE' 'common/dlctext.meta'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_buildings.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_int_247.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_int_plazabuilding.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_int_roadc_tunnel01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_int_sheriffoffice.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_int_underpass01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_land.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_props.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_road01.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnxchiliadtown_road03.ytyp'
---
+++ dnxnewcalafiaroads/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+files {
+	'common/*.meta'
+}
+data_file 'DLC_ITYP_REQUEST' 'stream/dnx_newcalafiaroads_land.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/dnx_newcalafiaroads_roadsigns.ytyp'
